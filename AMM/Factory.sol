a6820c91fdc775cc155942a45fee0a103a9a84f9
jaeuk2274 <jaeuk2274@gmail.com>
2024-04-01 00:01:09 +0900

    Update Factory.sol

--- AMM/Factory.sol
+++ AMM/Factory.sol
@@ -1,1 +1,23 @@
- 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+contract Exchange {
+    uint public number;
+    constructor(uint _number){
+        number = _number;
+    }
+}
+
+contract Factory {
+    mapping(uint => address) public numberToExchange;
+    mapping(address => uint) public exchangeToNumber;
+
+    function createExchange(uint number) public returns (address) {
+        Exchange created = new Exchange(number);
+        numberToExchange[number] = address(created);
+        exchangeToNumber[address(created)] = number;
+        // numberToExchange[123] = 0x...
+        // exchangeToNumber[0x...] = 123
+        return address(created);
+    }
+}
